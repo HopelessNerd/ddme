2cc9f6da127cfed09cd6de26b42d616df46242b1
Harsh <harshjoshi211@gmail.com>
2017-03-21 19:45:13 -0400

    Add a class for the configuration of the entity framework to override the existing configuration

---
+++ DbConnect/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.Migrations;
+
+namespace DbConnect
+{
+    internal class Configuration : DbMigrationsConfiguration<PrimaryDataContext>
+    {
+        public Configuration()
+        {
+            CodeGenerator = new CustomCodeGenerator();
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = false;
+        }
+    }
+}
