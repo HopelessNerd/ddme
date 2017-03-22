23638553f1f83d9cb8871517d56987b2884f38ef
Harsh <harshjoshi211@gmail.com>
2017-03-21 20:32:44 -0400

    Add primary data context for the migrations and add unit of work for the easy transactions

---
+++ DbConnect/PrimaryDataContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MySql.Data.Entity;
+using DbConnect.Poco;
+
+namespace DbConnect
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class PrimaryDataContext : DbContext
+    {
+        public PrimaryDataContext()
+            : base("Server=localhost;Port=3306;Database=ddme;Uid=root;Pwd=123456789")
+        {
+
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<TestResult> TestResult { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+            base.Configuration.LazyLoadingEnabled = false;
+            base.Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
---
+++ DbConnect/UnitOfWork.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data.Entity.Validation;
+using DbConnect.Poco;
+
+namespace DbConnect
+{
+    /// <summary>
+    /// Unit of Work class responsible for DB transactions
+    /// </summary>
+    public class UnitOfWork : IDisposable
+    {
+        #region Private member variables
+
+        private PrimaryDataContext _context = null;
+
+        #region Generic Repositories
+        private GenericRepository<User> _genericUserRepo;
+        #endregion
+
+        #region Specific Repository
+
+
+        #endregion
+        #endregion
+
+        public UnitOfWork()
+        {
+            _context = new PrimaryDataContext();
+        }
+
+        #region Public Repository Creation properties...
+
+        #region Generic Repositories
+        /// <summary>
+        /// Get/Set property user repository.
+        /// </summary>
+        public GenericRepository<User> GenericUserRepo
+        {
+            get
+            {
+                if (this._genericUserRepo == null)
+                    this._genericUserRepo = new GenericRepository<User>(_context);
+                return _genericUserRepo;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public member methods...
+        /// <summary>
+        /// Save method.
+        /// </summary>
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                    throw ex;
+            }
+        }
+
+        #endregion
+
+        #region Implementing IDiosposable...
+
+        #region private dispose variable declaration...
+        private bool disposed = false;
+        #endregion
+
+        /// <summary>
+        /// Protected Virtual Dispose method
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        /// <summary>
+        /// Dispose method
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
