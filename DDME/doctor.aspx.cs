e393c43e2a5a387df41230f2b2e8138816d33153
Harsh <harshjoshi211@gmail.com>
2017-03-25 21:34:07 -0400

    Add methods to populate controls and save data from the controls to the database for the Doctor form. Changes are done in the aspx and cs file Dropdown of the gender is changed to the radio button

--- DDME/doctor.aspx.cs
+++ DDME/doctor.aspx.cs
@@ -4,10 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DbConnect;
+using DbConnect.Poco;
+
 public partial class Register : System.Web.UI.Page
 {
+    #region Private Variables
+    private Doctor doctor = new Doctor();
+    UnitOfWork work = new UnitOfWork();
+    #endregion
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        GenericMethods genericMethods = new GenericMethods();
 
+        if (!string.IsNullOrEmpty((string)Session["UserId"]) && (string)Session["UserType"] == "Doctor")
+        {
+            doctor = work.GenericDoctorRepo.GetFirst(d => d.UserId == (int)Session["UserId"]);
+            FillControls();
+        }  
+
+    }
+
+    private void FillControls()
+    {
+        txtFirstName.Text = doctor.FirstName;
+        txtMiddleName.Text = doctor.MiddleName;
+        txtLastName.Text = doctor.LastName;
+
+        if (doctor.Gender == Gender.Male)
+            chkMale.Checked = true;
+        else
+            chkFemale.Checked = true;
+        txtAddress.Text = doctor.Address;
+        txtCountry.Text = doctor.Country;
+        txtMobile.Text = doctor.MobileNo;
+        txtAlternative.Text = doctor.AlternativeNo;
+
+        txtSpeciality.Text = doctor.Speciality;
+        txtQualifications.Text = doctor.Qualifications;
+    }
+
+    private void CacheDetails()
+    {
+        doctor.FirstName = txtFirstName.Text;
+        doctor.MiddleName = txtMiddleName.Text;
+        doctor.LastName = txtLastName.Text;
+        doctor.Gender = chkMale.Checked == true ? Gender.Male : chkFemale.Checked == true ? Gender.Female : Gender.Male;
+        doctor.Address = txtAddress.Text;
+        doctor.Country = txtCountry.Text;
+        doctor.MobileNo = txtMobile.Text;
+        doctor.AlternativeNo = txtAlternative.Text;
     }
 }
