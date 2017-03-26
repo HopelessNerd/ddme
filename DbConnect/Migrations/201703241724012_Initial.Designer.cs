99fcad996e99bcaf1e8fe76468538666584f5753
Harsh <harshjoshi211@gmail.com>
2017-03-25 21:34:50 -0400

    Update migrations after changing in the entities and add generic method class to the Dbconnect project

--- DbConnect/Migrations/201703241724012_Initial.Designer.cs
+++ DbConnect/Migrations/201703241724012_Initial.Designer.cs
@@ -13,7 +13,7 @@ namespace DbConnect
         
         string IMigrationMetadata.Id
         {
-            get { return "201703241644482_Initial"; }
+            get { return "201703241724012_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- DbConnect/Migrations/201703241724012_Initial.cs
+++ DbConnect/Migrations/201703241724012_Initial.cs
@@ -20,7 +20,50 @@ namespace DbConnect
                         doctorId = c.Int(nullable: false),
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Patient", t => t.doctorId)
+                .ForeignKey("dbo.Doctor", t => t.doctorId)
+                .ForeignKey("dbo.Patient", t => t.patientId)
+                .Index(t => t.patientId)
+                .Index(t => t.doctorId);
+            
+            CreateTable(
+                "dbo.Doctor",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 25, unicode: false),
+                        MiddleName = c.String(nullable: false, maxLength: 25, unicode: false),
+                        LastName = c.String(nullable: false, maxLength: 25, unicode: false),
+                        MobileNo = c.String(maxLength: 20, unicode: false),
+                        LandLineNo = c.String(maxLength: 50, unicode: false),
+                        AlternativeNo = c.String(maxLength: 50, unicode: false),
+                        Email = c.String(maxLength: 254, unicode: false),
+                        Address = c.String(maxLength: 150, unicode: false),
+                        Country = c.String(maxLength: 25, unicode: false),
+                        Gender = c.Int(nullable: false),
+                        Speciality = c.String(maxLength: 40, unicode: false),
+                        Qualifications = c.String(maxLength: 40, unicode: false),
+                        LastUpdatedDate = c.DateTime(nullable: false, precision: 0),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.User", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Event",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CreationDate = c.DateTime(nullable: false, precision: 0),
+                        StartTime = c.DateTime(nullable: false, precision: 0),
+                        EndTime = c.DateTime(nullable: false, precision: 0),
+                        Description = c.String(maxLength: 50, unicode: false),
+                        IsCreatedByDoctor = c.Boolean(nullable: false),
+                        patientId = c.Int(nullable: false),
+                        doctorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Doctor", t => t.doctorId)
                 .ForeignKey("dbo.Patient", t => t.patientId)
                 .Index(t => t.patientId)
                 .Index(t => t.doctorId);
@@ -65,30 +108,6 @@ namespace DbConnect
                 .Index(t => t.UserName, unique: true);
             
             CreateTable(
-                "dbo.Doctor",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        FirstName = c.String(nullable: false, maxLength: 25, unicode: false),
-                        MiddleName = c.String(nullable: false, maxLength: 25, unicode: false),
-                        LastName = c.String(nullable: false, maxLength: 25, unicode: false),
-                        MobileNo = c.String(maxLength: 20, unicode: false),
-                        LandLineNo = c.String(maxLength: 50, unicode: false),
-                        AlternativeNo = c.String(maxLength: 50, unicode: false),
-                        Email = c.String(maxLength: 254, unicode: false),
-                        Address = c.String(maxLength: 150, unicode: false),
-                        Country = c.String(maxLength: 25, unicode: false),
-                        Gender = c.Int(nullable: false),
-                        Speciality = c.String(maxLength: 40, unicode: false),
-                        Qualifications = c.String(maxLength: 40, unicode: false),
-                        LastUpdatedDate = c.DateTime(nullable: false, precision: 0),
-                        UserId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.User", t => t.UserId)
-                .Index(t => t.UserId);
-            
-            CreateTable(
                 "dbo.Pharmacist",
                 c => new
                     {
@@ -109,6 +128,25 @@ namespace DbConnect
                 .Index(t => t.UserId);
             
             CreateTable(
+                "dbo.Prescription",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Prescribe = c.String(maxLength: 50, unicode: false),
+                        CreationDate = c.DateTime(nullable: false, precision: 0),
+                        File1 = c.String(maxLength: 50, unicode: false),
+                        File2 = c.String(maxLength: 50, unicode: false),
+                        Note = c.String(maxLength: 50, unicode: false),
+                        patientId = c.Int(nullable: false),
+                        doctorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Doctor", t => t.doctorId)
+                .ForeignKey("dbo.Patient", t => t.patientId)
+                .Index(t => t.patientId)
+                .Index(t => t.doctorId);
+            
+            CreateTable(
                 "dbo.TestResult",
                 c => new
                     {
@@ -127,76 +165,38 @@ namespace DbConnect
                 .ForeignKey("dbo.Patient", t => t.patientId)
                 .Index(t => t.patientId);
             
-            CreateTable(
-                "dbo.Event",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        CreationDate = c.DateTime(nullable: false, precision: 0),
-                        StartTime = c.DateTime(nullable: false, precision: 0),
-                        EndTime = c.DateTime(nullable: false, precision: 0),
-                        Description = c.String(maxLength: 50, unicode: false),
-                        IsCreatedByDoctor = c.Boolean(nullable: false),
-                        patientId = c.Int(nullable: false),
-                        doctorId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Patient", t => t.doctorId)
-                .ForeignKey("dbo.Patient", t => t.patientId)
-                .Index(t => t.patientId)
-                .Index(t => t.doctorId);
-            
-            CreateTable(
-                "dbo.Prescription",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Prescribe = c.String(maxLength: 50, unicode: false),
-                        CreationDate = c.DateTime(nullable: false, precision: 0),
-                        File1 = c.String(maxLength: 50, unicode: false),
-                        File2 = c.String(maxLength: 50, unicode: false),
-                        Note = c.String(maxLength: 50, unicode: false),
-                        patientId = c.Int(nullable: false),
-                        doctorId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Patient", t => t.doctorId)
-                .ForeignKey("dbo.Patient", t => t.patientId)
-                .Index(t => t.patientId)
-                .Index(t => t.doctorId);
-            
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.Prescription", "FK_Prescription_Patient_patientId");
-            DropForeignKey("dbo.Prescription", "FK_Prescription_Patient_doctorId");
-            DropForeignKey("dbo.Event", "FK_Event_Patient_patientId");
-            DropForeignKey("dbo.Event", "FK_Event_Patient_doctorId");
             DropForeignKey("dbo.Appointment", "FK_Appointment_Patient_patientId");
-            DropForeignKey("dbo.Appointment", "FK_Appointment_Patient_doctorId");
+            DropForeignKey("dbo.Appointment", "FK_Appointment_Doctor_doctorId");
+            DropForeignKey("dbo.Doctor", "FK_Doctor_User_UserId");
+            DropForeignKey("dbo.Event", "FK_Event_Patient_patientId");
             DropForeignKey("dbo.TestResult", "FK_TestResult_Patient_patientId");
+            DropForeignKey("dbo.Prescription", "FK_Prescription_Patient_patientId");
+            DropForeignKey("dbo.Prescription", "FK_Prescription_Doctor_doctorId");
             DropForeignKey("dbo.Patient", "FK_Patient_User_UserId");
             DropForeignKey("dbo.Pharmacist", "FK_Pharmacist_User_UserId");
-            DropForeignKey("dbo.Doctor", "FK_Doctor_User_UserId");
+            DropForeignKey("dbo.Event", "FK_Event_Doctor_doctorId");
+            DropIndex("dbo.TestResult", new[] { "patientId" });
             DropIndex("dbo.Prescription", new[] { "doctorId" });
             DropIndex("dbo.Prescription", new[] { "patientId" });
-            DropIndex("dbo.Event", new[] { "doctorId" });
-            DropIndex("dbo.Event", new[] { "patientId" });
-            DropIndex("dbo.TestResult", new[] { "patientId" });
             DropIndex("dbo.Pharmacist", new[] { "UserId" });
-            DropIndex("dbo.Doctor", new[] { "UserId" });
             DropIndex("dbo.User", new[] { "UserName" });
             DropIndex("dbo.Patient", new[] { "UserId" });
+            DropIndex("dbo.Event", new[] { "doctorId" });
+            DropIndex("dbo.Event", new[] { "patientId" });
+            DropIndex("dbo.Doctor", new[] { "UserId" });
             DropIndex("dbo.Appointment", new[] { "doctorId" });
             DropIndex("dbo.Appointment", new[] { "patientId" });
-            DropTable("dbo.Prescription");
-            DropTable("dbo.Event");
             DropTable("dbo.TestResult");
+            DropTable("dbo.Prescription");
             DropTable("dbo.Pharmacist");
-            DropTable("dbo.Doctor");
             DropTable("dbo.User");
             DropTable("dbo.Patient");
+            DropTable("dbo.Event");
+            DropTable("dbo.Doctor");
             DropTable("dbo.Appointment");
         }
     }
