112e6e6c84bfc3796dc0fd6c34cab68f07731008
Dhrumit <dhrumit07@gmail.com>
2017-04-04 03:14:10 -0400

    code to display navigation mennu based on user session.

--- DDME/MasterPage.master.cs
+++ DDME/MasterPage.master.cs
@@ -9,6 +9,39 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["UserId"] != null && (string)Session["UserType"] == "Patient")
+        {
+            patientLogin.Visible = true;
+            DoctorLogin.Visible = false;
+            nologin.Visible = false;
+            PharmacistLogin.Visible = false;
+
+        }
+        else if (Session["UserId"] != null && (string)Session["UserType"] == "Doctor")
+        {
+            patientLogin.Visible = false;
+            DoctorLogin.Visible = true;
+            nologin.Visible = false;
+            PharmacistLogin.Visible = false;
+
+        }
+        else if (Session["UserId"] != null && (string)Session["UserType"] == "Pharmacist")
+        {
+            patientLogin.Visible = false;
+            DoctorLogin.Visible = false;
+            nologin.Visible = false;
+            PharmacistLogin.Visible = true;
+
+        }
+        else
+        {          
+                patientLogin.Visible = false;
+                DoctorLogin.Visible = false;
+                nologin.Visible = true;
+                PharmacistLogin.Visible = false;
+
+         }
+
 
     }
 }
